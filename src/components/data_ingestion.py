6db39bf71faa90e2e2e04ae7b7374b7d0cc3bff0
namitha9eshan <namithaeshanpethiyagoda@gmail.com>
2026-08-25 15:32:46 +0530

    Hyperparameter turning

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,6 +5,7 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 from src.components.model_trainer import ModelTrainer
--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import pandas as pd
 import dill
 from sklearn.metrics import r2_score
 from src.exception import CustomerException
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -19,14 +20,22 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomerException(e,sys)
 
-def evaluate_model(X_train, y_train, X_test, y_test, models):
+def evaluate_model(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            # Train model
+            para = params[list(models.keys())[i]] #list(models.keys())[i]
+
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
+
+            # Train model
+            #model.fit(X_train, y_train)
 
             # Predict Testing data
             y_train_pred = model.predict(X_train)
